088bfe56eb666b852c77b56d2b81d46b27442300
Tyler Renelle <tylerrenelle@gmail.com>
2012-03-14 15:47:34 -0400

    Initial commit

---
+++ sharpe_calculator.js
@@ -0,0 +1,22 @@
+(function ($) {
+  Drupal.behaviors.sharpe_calculator = {
+    attach: function(context) {
+      
+      $('.sharpe-jeditable', context).editable(function(value, settings) {
+        search_id = "#" + this.id.replace('inline', 'input');
+        $input = $("#sharpe-calc-form "+search_id+" :input");
+        console.log($input);
+        $input.val(value);
+        $('#sharpe-calc-form :submit').mousedown(); // see http://goo.gl/zwNdt
+        return(value);
+       }, { 
+        indicator : 'Saving...',
+        tooltip   : 'Click to edit...',
+        cancel    : 'Cancel',
+        submit    : 'Save',
+        style     : 'display: inline; min-width: 200px;'
+      });
+      
+    }
+  };
+})(jQuery);
